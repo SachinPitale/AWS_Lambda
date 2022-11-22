b748d699ebf2040f0c453c3034c33a87c844c5c6
SachinPitale <sachinpitale22@gmail.com>
2022-11-22 12:19:24 +0530

    updated script

--- 02-list-s3-bucket.py
+++ 02-list-s3-bucket.py
@@ -3,5 +3,6 @@ import boto3
 aws_con=boto3.session.Session()
 list_s3=aws_con.resource('s3')
 
-for i in list_s3.buckets_all():
-    print(i)
+print(list_s3)
+# for i in list_s3.buckets_all():
+#     print(i)
