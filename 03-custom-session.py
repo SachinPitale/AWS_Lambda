97d265e15027253abe7fa831f40b054235ab5ead
SachinPitale <sachinpitale22@gmail.com>
2022-11-23 13:11:17 +0530

    updated script

--- 03-custom-session.py
+++ 03-custom-session.py
@@ -1,5 +1,5 @@
 import boto3
 
 aws_con=boto3.session.Session()
-iam_res=aws_con.resource(servie_name='iam', region_name="us-east-1")
-iam_con=aws_con.client(service_name='iam', region_name='us_east-1')
+iam_res=aws_con.resource(servie_name='iam',region_name="us-east-1")
+iam_con=aws_con.client(service_name='iam',region_name="us_east-1")
