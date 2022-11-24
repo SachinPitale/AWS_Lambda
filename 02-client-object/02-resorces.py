ec71077f538493ae90da9912aa9b2fb12a7d90ee
SachinPitale <sachinpitale22@gmail.com>
2022-11-24 13:20:38 +0530

    updated script

--- 02-client-object/02-resorces.py
+++ 02-client-object/02-resorces.py
@@ -2,6 +2,7 @@ import boto3
 
 aws_console=boto3.session.Session()
 iam_res=aws_console.resource('iam', region_name="us-east-1")
+s3_res=aws_console.resource('s3', region_name="us-east-1")
 
 for each_item in iam_res.users.all():
     print(each_item)
@@ -13,3 +14,11 @@ for each_item in iam_res.users.all():
 for each_item in iam_res.users.limit(1):
     print(each_item)
     print(each_item.user_name)
+
+
+for each_item in s3_res.buckets.all():
+    print(each_item.name)
+
+
+for each_item in s3_res.buckets.limit(10):
+    print(each_item.name)
