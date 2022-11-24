cab8f1e88ba25926731d5519664b1ef404ffa96c
SachinPitale <sachinpitale22@gmail.com>
2022-11-24 12:11:58 +0530

    updated script

--- 02-client-object/01-client.py
+++ 02-client-object/01-client.py
@@ -6,7 +6,7 @@ iam_client=aws_console.client('iam', region_name='us-east-1')
 e2_client=aws_console.client('ec2', region_name='us-east-1')
 s3_client=aws_console.client('s3', region_name='us-east-1')
 
-
+## List IAM user
 iam_response=iam_client.list_users()
 print(iam_response)
 
@@ -19,3 +19,11 @@ for each_iteam in iam_response['Users']:
 for each_iteam in iam_response['Users']:
     print(each_iteam['UserName'])
 
+## List EC2 instances
+
+e2_response=e2_client.describe_instances()
+print(e2_response)
+
+for each_item in e2_response['Reservations']:
+    for each_instance in each_item['Instances']:
+        print(each_instance['InstanceId'])
