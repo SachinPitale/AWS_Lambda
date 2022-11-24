85335b056faac2a1a6db41fc34e3d1a701f46d48
SachinPitale <sachinpitale22@gmail.com>
2022-11-24 13:12:30 +0530

    updated script

--- 02-client-object/02-resorces.py
+++ 02-client-object/02-resorces.py
@@ -6,5 +6,5 @@ iam_res=aws_console.resource('iam', region_name="us-east-1")
 for each_item in iam_res.users.all():
     print(each_item)
     print(dir(each_item))
-    print(each_item['user_name'])
-    print(each_item['AccessKey'])
+    print(each_item.user_name)
+    print(each_item.AccessKey)
