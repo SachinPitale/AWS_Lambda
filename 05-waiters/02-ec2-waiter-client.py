ab75db1b52038780eb044b5701a1115ced63501c
SachinPitale <sachinpitale22@gmail.com>
2022-11-25 12:59:45 +0530

    updated script

--- 05-waiters/02-ec2-waiter-client.py
+++ 05-waiters/02-ec2-waiter-client.py
@@ -4,6 +4,6 @@ import time
 aws_console=boto3.session.Session()
 ec2_console=aws_console.client('ec2', region_name='us-east-1')
 
-ec2_console.start_instances(instance_id=['i-06186ed7e182cc28b'])
+ec2_console.start_instances(InstanceIds=['i-06186ed7e182cc28b'])
 waiter = ec2_console.get_waiter('instance_running')
 print("Ec2 instance is running")
