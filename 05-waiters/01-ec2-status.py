f4730164cd16480576d876607fbf259fc936eaa3
SachinPitale <sachinpitale22@gmail.com>
2022-11-25 12:01:31 +0530

    updated script

--- 05-waiters/01-ec2-status.py
+++ 05-waiters/01-ec2-status.py
@@ -6,5 +6,5 @@ aws_console=boto3.session.Session()
 ec2_console=aws_console.client('ec2', region_name='us-east-1')
 
 #ec2_console.start_instances(instance_id='i-06186ed7e182cc28b')
-response=ec2_console.describe_instances(instance_id='i-06186ed7e182cc28b')
+response=ec2_console.describe_instances(InstanceIds='i-06186ed7e182cc28b')
 pprint(response)
