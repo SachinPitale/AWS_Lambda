87570213006fa569b1c9218921f8b8476334bf40
SachinPitale <sachinpitale22@gmail.com>
2022-11-25 13:03:43 +0530

    updated script

--- 05-waiters/02-ec2-waiter-client.py
+++ 05-waiters/02-ec2-waiter-client.py
@@ -5,6 +5,7 @@ aws_console=boto3.session.Session()
 ec2_console=aws_console.client('ec2', region_name='us-east-1')
 
 ec2_console.start_instances(InstanceIds=['i-06186ed7e182cc28b'])
+print("Instance is pending state")
 waiter = ec2_console.get_waiter('instance_running')
 waiter.wait(InstanceIds=['i-06186ed7e182cc28b'])
 print("Ec2 instance is running")
