71ef7bb7f14893bb78e90e8048db0ea21d6202ea
SachinPitale <sachinpitale22@gmail.com>
2022-11-25 13:06:54 +0530

    updated script

--- 05-waiters/03-ec2-waiter-resources.py
+++ 05-waiters/03-ec2-waiter-resources.py
@@ -6,7 +6,7 @@ ec2_con_re=aws_con.resource('ec2',region_name="us-east-1")
 ec2_con_cli=aws_con.client(service_name="ec2",region_name="us-east-1")
 
 
-my_inst_ob=ec2_con_re.Instance("i-002d4110f1199166f")
+my_inst_ob=ec2_con_re.Instance("i-06186ed7e182cc28b")
 print("Starting given instance....")
 my_inst_ob.start()
 my_inst_ob.wait_until_running()  #Resource waiter waits for 200sec(40 checks after every 5 sec)
