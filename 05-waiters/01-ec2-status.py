92f1839ab1848bc880a34bc3e2b98572e6809f0d
SachinPitale <sachinpitale22@gmail.com>
2022-11-25 12:08:32 +0530

    updated script

--- 05-waiters/01-ec2-status.py
+++ 05-waiters/01-ec2-status.py
@@ -8,4 +8,6 @@ ec2_console=aws_console.client('ec2', region_name='us-east-1')
 #ec2_console.start_instances(instance_id=['i-06186ed7e182cc28b'])
 response=ec2_console.describe_instances(InstanceIds=['i-06186ed7e182cc28b'])
 #pprint(response)
-print(response['Reservations'])
+#print(response['Reservations'])
+for each_iteam in (response['Reservations']):
+    print(each_iteam)
