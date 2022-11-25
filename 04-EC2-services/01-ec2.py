bd82081aa8b6a245f0370dac6cc9cb5ff31978ab
SachinPitale <sachinpitale22@gmail.com>
2022-11-25 10:50:25 +0530

    updated script

--- 04-EC2-services/01-ec2.py
+++ 04-EC2-services/01-ec2.py
@@ -4,19 +4,19 @@ from pprint import pprint
 aws_console=boto3.session.Session()
 ec2_client=aws_console.client('ec2', region_name='us-east-1')
 
-response=ec2_client.describe_instances()
-pprint(response)
-
-response=ec2_client.describe_instances()['Reservations']
-pprint(response)
-
-# response=ec2_client.describe_instances()['ResponseMetadata']
+# response=ec2_client.describe_instances()
 # pprint(response)
-
 
 # response=ec2_client.describe_instances()['Reservations']
 # pprint(response)
 
-# for each_item in response:
-#     for each in each_item['Instances']:
-#         print("The Image Id is: {}\nThe Instance Id Is: {}\nThe Instance Launch Time is: {}".format(each['ImageId'],each['InstanceId'],each['LaunchTime'].strftime("%Y-%m-%d")))
+# # response=ec2_client.describe_instances()['ResponseMetadata']
+# # pprint(response)
+
+
+response=ec2_client.describe_instances()['Reservations']
+pprint(response)
+
+for each_item in response:
+    for each in each_item['Instances']:
+        print("The Image Id is: {}\nThe Instance Id Is: {}\nThe Instance Launch Time is: {}".format(each['ImageId'],each['InstanceId'],each['LaunchTime'].strftime("%Y-%m-%d")))
