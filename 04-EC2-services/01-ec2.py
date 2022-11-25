be8ba0ca56eb1e9f89a0bfe5e736ac5c0ef78da7
SachinPitale <sachinpitale22@gmail.com>
2022-11-25 10:35:58 +0530

    updated script

--- 04-EC2-services/01-ec2.py
+++ 04-EC2-services/01-ec2.py
@@ -6,3 +6,6 @@ ec2_client=aws_console.client('ec2', region_name='us-east-1')
 
 response=ec2_client.describe_instances()
 pprint(response)
+
+response=ec2_client.describe_instances()['Reservations']
+pprint(response)
