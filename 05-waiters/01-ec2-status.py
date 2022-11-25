0ee4d7d82e7a7482d67fbda27f64e3d629989145
SachinPitale <sachinpitale22@gmail.com>
2022-11-25 12:00:40 +0530

    updated script

---
+++ 05-waiters/01-ec2-status.py
@@ -0,0 +1,10 @@
+import boto3
+from pprint import pprint
+
+
+aws_console=boto3.session.Session()
+ec2_console=aws_console.client('ec2', region_name='us-east-1')
+
+#ec2_console.start_instances(instance_id='i-06186ed7e182cc28b')
+response=ec2_console.describe_instances(instance_id='i-06186ed7e182cc28b')
+pprint(response)
