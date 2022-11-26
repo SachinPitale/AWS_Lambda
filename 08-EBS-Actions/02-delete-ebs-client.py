ccfd71f34543c6bb173fea970076070775768401
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 10:50:43 +0530

    updated script

--- 08-EBS-Actions/02-delete-ebs-client.py
+++ 08-EBS-Actions/02-delete-ebs-client.py
@@ -3,5 +3,5 @@ import boto3
 aws_console=boto3.session.Session()
 ec2_client=aws_console.client('ec2')
 
-response=ec2_client.describe_volumes()
+response=ec2_client.describe_volumes()['Volumes']
 print(response)
