21e2fb740047083aff00c83b3c83d677ed871092
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 11:41:33 +0530

    updated script

--- 08-EBS-Actions/04-get-ec2-inventory.py
+++ 08-EBS-Actions/04-get-ec2-inventory.py
@@ -1,10 +1,16 @@
 import boto3
+import csv
 
+csv_ob=open("inventory_info.csv","w",newline='')
+csv_w=csv.writer(csv_ob)
+csv_w.writerow(["S_NO","Instance_Id",'Instance_Type','Architecture','LaunchTime','Privat_Ip'])
 aws_console=boto3.session.Session()
-
 ec2_res=aws_console.resource('ec2')
-
 response=ec2_res.instances.all()
 
 for each in response:
-    print(each.instance_id,each.instance_type,each.architecture,each.launch_time.strftime("%Y-%m-%d"),each.private_ip_address,each.tags)
+    print(each.instance_id,each.instance_type,each.architecture,each.launch_time.strftime("%Y-%m-%d"),each.private_ip_address)
+    csv_w.writerow([cnt,each.instance_id,each.instance_type,each.architecture,each.launch_time.strftime("%Y-%m-%d"),each.private_ip_address])
+
+csv_ob.close()
+    
