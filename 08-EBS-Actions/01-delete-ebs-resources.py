6bcf238203a9352e921bdcfcae944adfb7808ecd
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 10:36:04 +0530

    updated script

--- 08-EBS-Actions/01-delete-ebs-resources.py
+++ 08-EBS-Actions/01-delete-ebs-resources.py
@@ -2,7 +2,7 @@ import boto3
 
 aws_console=boto3.session.Session()
 ec2_con_res=aws_console.resource('ec2')
-
-response=ec2_con_res.volumes.all()
+ebs_filter={"Name":"status","Value":['available']}
+response=ec2_con_res.volumes.filter(filter=[ebs_filter])
 for each_iteam in response:
-    print(each_iteam)
+    print(each_iteam.tags,each_iteam.id,each_iteam.state)
