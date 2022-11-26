07ff8168002df4d3b2bad83c67befa24dc517170
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 12:45:11 +0530

    added script

--- 10-IAM/02-get-group.py
+++ 10-IAM/02-get-group.py
@@ -8,6 +8,5 @@ iam_con_re=session.resource("iam")
 iam_user_ob=iam_con_re.User("s3_developer")
 print(iam_user_ob.user_name,iam_user_ob.user_id,iam_user_ob.arn,iam_user_ob.create_date.strftime("%Y-%m-%d"))
 '''
-for iam_user_ob in iam_con_re.users.all():
-	print(iam_user_ob)
-	print(iam_user_ob.user_name,iam_user_ob.user_id,iam_user_ob.arn,iam_user_ob.create_date.strftime("%Y-%m-%d"))
+for iam_group_ob in iam_con_re.group.all():
+	print(iam_group_ob)
