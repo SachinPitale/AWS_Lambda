6ce02f6dffa1a6230e228b450aab35a612d58f87
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 10:55:07 +0530

    updated script

--- 08-EBS-Actions/02-delete-ebs-client.py
+++ 08-EBS-Actions/02-delete-ebs-client.py
@@ -6,4 +6,4 @@ ec2_client=aws_console.client('ec2')
 #esponse=ec2_client.describe_volumes()['Volumes']
 #print(response)
 for each_item in ec2_client.describe_volumes()['Volumes']:
-    print(each_item['VolumeId'], each_item['State'],each_item['Tags'])
+    print(each_item['VolumeId'], each_item['State'], each_item['Tags'])
