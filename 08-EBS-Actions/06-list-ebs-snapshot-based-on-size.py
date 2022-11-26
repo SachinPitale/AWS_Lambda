462a6e8ee4b7da5d3d90d264141dc973add90175
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 11:59:15 +0530

    updated script

--- 08-EBS-Actions/06-list-ebs-snapshot-based-on-size.py
+++ 08-EBS-Actions/06-list-ebs-snapshot-based-on-size.py
@@ -2,8 +2,8 @@ import boto3
 aws_mag_con=boto3.session.Session()
 ec2_con_re=aws_mag_con.resource('ec2')
 
-sts_con_cli=aws_mag_con.client()
-response=sts_con_cli.get_caller_identity('sts')
+sts_con_cli=aws_mag_con.client('sts')
+response=sts_con_cli.get_caller_identity()
 my_own_id=response.get('Account')
 f_size={"Name":"volume-size","Values":['10']}
 for each_snap in ec2_con_re.snapshots.filter(OwnerIds=[my_own_id],Filters=[f_size]):
