482a26810c05e219c52caeb8c7ba34168d0c3c52
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 11:05:16 +0530

    updated script

--- 08-EBS-Actions/02-delete-ebs-client.py
+++ 08-EBS-Actions/02-delete-ebs-client.py
@@ -3,12 +3,18 @@ import boto3
 aws_console=boto3.session.Session()
 ec2_client=aws_console.client('ec2')
 
-#esponse=ec2_client.describe_volumes()['Volumes']
+#response=ec2_client.describe_volumes()['Volumes']
 #print(response)
-for each_item in ec2_client.describe_volumes()['Volumes']:
-    print(each_item['VolumeId'], each_item['State'])
-    try:
-        if each_item['Tags']:
-            print(each_item['Tags'])
-    except:
-        print(f"Tags is not avaliable for {each_item['VolumeId']}")
+# for each_item in ec2_client.describe_volumes()['Volumes']:
+#     print(each_item['VolumeId'], each_item['State'])
+#     try:
+#         if each_item['Tags']:
+#             print(each_item['Tags'])
+#     except:
+#         print("Tags is not avaliable for {each_item['VolumeId']}")
+
+
+response=ec2_client.describe_volumes()['Volumes']
+for each_item in response:
+    if not "Tags" in each_item and each_item['State']=='available':
+        print(each_item['VolumeId'], each_item['State'])
