117e08bcacb4fcd107e23aec5b09b75f7d8a851a
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 12:41:09 +0530

    updated script

--- 10-IAM/01-get-user.py
+++ 10-IAM/01-get-user.py
@@ -4,7 +4,7 @@ session=boto3.session.Session()
 iam_con_re=session.resource("iam")
 #Get details of any iam user
 
-iam_user_ob=iam_con_re.User("s3_developer")
+iam_user_ob=iam_con_re.User()
 print(iam_user_ob.user_name,iam_user_ob.user_id,iam_user_ob.arn,iam_user_ob.create_date.strftime("%Y-%m-%d"))
 
 # for iam_user_ob in iam_con_re.users.all():
