91ae3affb63e43fd6879c0747abc79c8e1a4cc84
SachinPitale <sachinpitale22@gmail.com>
2022-11-26 11:39:47 +0530

    updated script

--- 08-EBS-Actions/04-get-ec2-inventory.py
+++ 08-EBS-Actions/04-get-ec2-inventory.py
@@ -7,5 +7,4 @@ ec2_res=aws_console.resource('ec2')
 response=ec2_res.instances.all()
 
 for each in response:
-    print(each)
-    print(dir(each))
+    print(each.instance_id,each.instance_type,each.architecture,each.launch_time.strftime("%Y-%m-%d"),each.private_ip_address,each.tags)
